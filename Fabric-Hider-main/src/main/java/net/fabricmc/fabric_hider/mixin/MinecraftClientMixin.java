ce6dfee6d17a74535e6fde4fb89f49a9f58a3e6a
TrollTaylor <taylordude145@gmail.com>
2022-05-05 16:05:34 -0500

    Hides fabric resource pack

---
+++ Fabric-Hider-main/src/main/java/net/fabricmc/fabric_hider/mixin/MinecraftClientMixin.java
@@ -0,0 +1,56 @@
+package net.fabricmc.fabric_hider.mixin;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.Window;
+import net.minecraft.util.ModStatus;
+import org.spongepowered.asm.mixin.*;
+
+@Mixin({MinecraftClient.class})
+public abstract class MinecraftClientMixin {
+
+
+    @Shadow @Final
+    private String gameVersion = "1.18.2";
+    
+	@Shadow @Final
+    private String versionType = "release";
+
+
+    @Shadow public abstract Window getWindow();
+
+    @Overwrite
+    public static ModStatus getModStatus() {
+        return new ModStatus(net.minecraft.util.ModStatus.Confidence.PROBABLY_NOT, "Client" + " jar signature and brand is untouched");
+    }
+
+    @Overwrite
+    public void updateWindowTitle() {
+        MinecraftClient.getInstance().getWindow().setTitle(getWindowTitle());
+    }
+
+    @Overwrite
+    private String getWindowTitle() {
+        StringBuilder stringBuilder = new StringBuilder("Minecraft");
+
+        stringBuilder.append(" ");
+        stringBuilder.append(SharedConstants.getGameVersion().getName());
+        ClientPlayNetworkHandler clientPlayNetworkHandler = MinecraftClient.getInstance().getNetworkHandler();
+        if (clientPlayNetworkHandler != null && clientPlayNetworkHandler.getConnection().isOpen()) {
+            stringBuilder.append(" - ");
+            if (MinecraftClient.getInstance().getServer() != null && !MinecraftClient.getInstance().getServer().isRemote()) {
+                stringBuilder.append(I18n.translate("title.singleplayer", new Object[0]));
+            } else if (MinecraftClient.getInstance().isConnectedToRealms()) {
+                stringBuilder.append(I18n.translate("title.multiplayer.realms", new Object[0]));
+            } else if (MinecraftClient.getInstance().getServer() == null && (MinecraftClient.getInstance().getCurrentServerEntry() == null || !MinecraftClient.getInstance().getCurrentServerEntry().isLocal())) {
+                stringBuilder.append(I18n.translate("title.multiplayer.other", new Object[0]));
+            } else {
+                stringBuilder.append(I18n.translate("title.multiplayer.lan", new Object[0]));
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
+
---
+++ Fabric-Hider-main/src/main/java/net/fabricmc/fabric_hider/mixin/MixinDebugHud.java
@@ -0,0 +1,57 @@
+package net.fabricmc.fabric_hider.mixin;
+
+import net.fabricmc.fabric.api.renderer.v1.RendererAccess;
+import net.minecraft.client.gui.hud.DebugHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(DebugHud.class)
+public class MixinDebugHud {
+
+    @Inject(at = @At("RETURN"), method = "getLeftText", cancellable =true)
+    protected void getLeftText(CallbackInfoReturnable<List<String>> info) {
+
+        info.cancel();
+
+        if (RendererAccess.INSTANCE.hasRenderer()) {
+            info.getReturnValue().remove("[Fabric] Active renderer: " + RendererAccess.INSTANCE.getRenderer().getClass().getSimpleName());
+        } else {
+            info.getReturnValue().remove("[Fabric] Active renderer: none (vanilla)");
+        }
+		
+        List renderList = new ArrayList<String>();
+        for(int i = 0; i < info.getReturnValue().size(); i++){
+            if(!(info.getReturnValue().get(i).contains("[Iris]") || info.getReturnValue().get(i).contains("[Entity Batching]"))){
+                renderList.add(info.getReturnValue().get(i));
+            }
+        }
+        if(renderList.size() >= 22) {
+            renderList.remove(renderList.size() - 1);
+            renderList.remove(renderList.size() - 1);
+        }
+        info.setReturnValue(renderList);
+    }
+	
+	
+    @Inject(at = @At("RETURN"), method = "getRightText", cancellable = true)
+    protected void getRightText(CallbackInfoReturnable<List<String>> info) {
+		
+        List renderList = new ArrayList<String>();
+        for(int i = 0; i < info.getReturnValue().size(); i++){
+            if(!(info.getReturnValue().get(i).contains("[Iris]") || info.getReturnValue().get(i).contains("[Entity Batching]") || info.getReturnValue().get(i).contains("Sodium") || info.getReturnValue().get(i).contains("IRIS") || info.getReturnValue().get(i).contains("Direct Buffers") || info.getReturnValue().get(i).contains("Off-Heap") || info.getReturnValue().get(i).contains("Device") || info.getReturnValue().get(i).contains("Chunk arena") || info.getReturnValue().get(i).contains("build") || info.getReturnValue().get(i).contains("buffer") || info.getReturnValue().get(i).contains("replaymod") || info.getReturnValue().get(i).contains("[Culling]") )){
+                renderList.add(info.getReturnValue().get(i));
+            }
+        }
+        if(renderList.size() >= 22) {
+            renderList.remove(9);
+			
+        }
+        info.setReturnValue(renderList);
+		
+    }
+}
